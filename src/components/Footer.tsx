96e023c4ecfe207157d55e5ecf019393140fe48a
Phrojekt <phrojekt22@gmail.com>
2023-08-20 01:11:37 -0300

    NewFooter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+import Image from "next/image"
+import LinkedinIcon from "../images/LinkedinButton.png"
+import GitHubIcon from "../images/GithubButton.png"
+import CurriculumIcon from "../images/CurriculumButton.png"
+import EmailIcon from "../images/EmailButton.png"
+
 export function Footer() {
   return (
-    <div className="m-0 pl-[110px] pt-[20px] pb-[20px] bg-footer-purple">
-      <div className='text-green text-2xl'>@Phrojekt | Paulo Henrique</div>
+    <div className="flex justify-between items-center m-0 pl-[30px] pr-[30px] pt-[10px] pb-[10px] bg-dark-purple sticky bottom-0 left-0">
+      <div className='text-green font-bold font-sans text-center'> &copy; Phrojekt | Paulo Henrique</div>
+      <div className="flex gap-x-1">
+        <Link href="www.linkedin.com/in/phrojektdev" target="_blank" rel="noreferrer">
+          <Image src={LinkedinIcon} alt="LinkedinIcon" className="max-w-[25px]"></Image>
+        </Link> 
+        <Link href="https://github.com/Phrojekt" target="_blank" rel="noreferrer">
+          <Image src={GitHubIcon} alt="LinkedinIcon" className="max-w-[25px]"></Image>
+        </Link> 
+        <Link href="/cv.pdf" target="_blank" rel="noreferrer">
+          <Image src={CurriculumIcon} alt="LinkedinIcon" className="max-w-[25px]"></Image>
+        </Link> 
+        <Link href="mailto:rick.p.sales@hotmail.com" target="_blank" rel="noreferrer">
+            <Image src={EmailIcon} alt="LinkedinIcon" className="max-w-[25px]"></Image>
+        </Link> 
+      </div>
+
     </div>
   )
 }
