cdf08aece0827955805afe6c9778cc92e6e66fe7
Phrojekt <101226915+Phrojekt@users.noreply.github.com>
2023-11-20 15:08:27 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ import EmailIcon from "../images/EmailButton.png"
 
 export function Footer() {
   return (
-    <div className="flex justify-between items-center m-0 pl-[30px] pr-[30px] pt-[10px] pb-[10px] bg-dark-purple sticky bottom-0 left-0">
+    <div className="flex justify-between items-center m-0 pl-[30px] pr-[30px] pt-[10px] pb-[10px] bg-dark-purple fixed bottom-0 left-0">
       <div className='text-green font-bold font-sans text-center'> &copy; Phrojekt | Paulo Henrique</div>
       <div className="flex gap-x-1">
         <Link href="www.linkedin.com/in/phrojektdev" target="_blank" rel="noreferrer">
