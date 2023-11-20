755fc8a324490223e9ff50c0db72a12d1579d6ef
Phrojekt <101226915+Phrojekt@users.noreply.github.com>
2023-11-20 15:02:42 -0300

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -36,7 +36,7 @@ export function Projects() {
         <div className="relative flex justify-center group sm:w-[300px] md:w-[460px] lg:w-[520px] xl:w-[80%] rounded-[10px]  bg-Rental-Gray p-5">   
           <Image src={RentalProject} alt="rentalMobilisticProject"/>
           <div className="w-full h-full absolute left-0 top-0 bg-overlay-purple/90 group-hover:visible invisible rounded-[10px] delay-150">
-            <div className="flex flex-col gap-y-4 text-white pl-5">
+            <div className="flex flex-col sm:gap-y-1 md:gap-y-4 text-white pl-5">
               <div className="sm:text-xl md:text-3xl font-bold pt-5">Rental Mobilistic</div>
               <div className="md:text-lg font-semibold">HTML, CSS & JavaScript</div>
               <div className="sm:pt-0 sm:text-xs md:text-base md:pt-4 sm:w-[80%] md:w-[60%]">
